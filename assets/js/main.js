b59b6b6a00022ffb2c1a39d6e3bc05a2e2536fd3
UnsalOztorun <unsaloztorun.developer@gmail.com>
2020-06-21 13:38:16 -0700

    fuk preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,7 @@
 !(function($) {
   "use strict";
 
-  // Preloader
-  $(window).on('load', function() {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
-        $(this).remove();
-      });
-    }
-  });
+ 
 
   
   // Hero typed
